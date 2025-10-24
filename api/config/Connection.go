4ade0a883964a9f204bd78b27a0cc59adebdc88c
devinluize <71080055+devinluize@users.noreply.github.com>
2025-10-24 08:45:30 +0700

    add

--- api/config/Connection.go
+++ api/config/Connection.go
@@ -12,7 +12,6 @@ import (
 	log "github.com/sirupsen/logrus"
 	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 )
 
@@ -71,48 +70,6 @@ func InitTestDB() *gorm.DB {
 }
 func InitDB() *gorm.DB {
 	return &gorm.DB{}
-	val := url.Values{}
-	val.Add("parseTime", "True")
-	val.Add("loc", "Asia/Jakarta")
-	//	dsn := fmt.Sprintf("sqlserver://localhost:1433?database=%s&connection+timeout=30&encrypt=disable&trustServerCertificate=false&app name=SqlClient", EnvConfigs.DBName)
-	//dsn := fmt.Sprintf("sqlserver://%s:%d?database=%s&connection+timeout=30&encrypt=disable&trustServerCertificate=false&app name=SqlClient",
-	//	EnvConfigs.Hostname,
-	//	EnvConfigs.DBPort,
-	//	EnvConfigs.DBName)
-	dsn := fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&connection+timeout=30&encrypt=disable&trustServerCertificate=false&app name=SqlClient",
-		EnvConfigs.DBUser,
-		EnvConfigs.DBPass,
-		EnvConfigs.DBHost,
-		EnvConfigs.DBPort,
-		EnvConfigs.DBName)
-	//dsn := "sqlserver://localhost:1433?database=assignmentDB&connection+timeout=30&encrypt=disable&trustServerCertificate=false&app name=SqlClient"
-	db, err := gorm.Open(sqlserver.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-		NamingStrategy: schema.NamingStrategy{
-			//TablePrefix:   "dbo.", // schema name
-			SingularTable: false,
-		}})
-	if err != nil {
-		log.Fatal("Cannot connected database ", err)
-		return nil
-	}
-	sqlDB, _ := db.DB()
-
-	err = sqlDB.Ping()
-
-	if err != nil {
-		log.Fatal("Request Timeout ", err)
-		return nil
-	}
-
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetConnMaxIdleTime(time.Minute * 3)
-	sqlDB.SetMaxOpenConns(20)
-	sqlDB.SetConnMaxLifetime(time.Minute * 3)
-
-	log.Info("Connected Database " + EnvConfigs.DBDriver + "- Connected on : " + EnvConfigs.DBHost)
-
-	return db
 }
 
 func InitCloudinary() *cloudinary.Cloudinary {
